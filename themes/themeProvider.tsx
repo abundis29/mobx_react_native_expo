131bdd58be72ef8071fe5009fad08db69601bc78
Ivan Abundis <heysoyivan@gmail.com>
2019-10-11 02:12:53 -0500

    Async change + Remove false async

--- themes/themeProvider.tsx
+++ themes/themeProvider.tsx
@@ -1,18 +1,30 @@
 // themeProvider.js
 
 import THEMES from "./themes.json";
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+// import AsyncStorage from '@react-native-community/async-storage';
+import { AsyncStorage } from 'react-native';
 
 export const themes = THEMES;
+
+const STORAGE_KEY = 'THEME_ID';
 
 const ThemeContext = React.createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-  const [themeID, setThemeID] = useState(THEMES[1].key);
+  const [themeID, setThemeID] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const storedThemeID = await AsyncStorage.getItem(STORAGE_KEY);
+      if (storedThemeID) setThemeID(storedThemeID);
+      else setThemeID(THEMES[1].key);
+    })();
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ themeID, setThemeID }}>
-      {children}
+      {!!themeID ? children : null}
     </ThemeContext.Provider>
   );
 };
@@ -21,7 +33,10 @@ export function withTheme(Component) {
   return props => {
     const { themeID, setThemeID } = useContext(ThemeContext);
     const getTheme = themeID => THEMES.find(theme => theme.key === themeID);
-    const setTheme = themeID => setThemeID(themeID);
+    const setTheme = themeID => {
+      AsyncStorage.setItem(STORAGE_KEY, themeID);
+      setThemeID(themeID);
+    };
     return (
       <Component
         {...props}
